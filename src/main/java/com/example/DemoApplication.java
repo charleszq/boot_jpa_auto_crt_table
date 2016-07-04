dbcf769ddb448d479cb7bc27de8733b9d1857e8a
Charles Zhang <charleszq@gmail.com>
2016-07-04 17:45:14 +0800

    olingo2 done

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,19 +1,40 @@
 package com.example;
 
+import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(DemoApplication.class);
+    }
+
+    @Bean
+    public CXFNonSpringJaxrsServlet odataServiceDispatcherServlet() {
+        return new CXFNonSpringJaxrsServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(odataServiceDispatcherServlet(), "/sampleodata2/*");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("javax.ws.rs.Application", "org.apache.olingo.odata2.core.rest.app.ODataApplication");
+        params.put("org.apache.olingo.odata2.service.factory", "com.example.service.MyServiceFactory");
+        registration.setInitParameters(params);
+        return registration;
     }
 }
